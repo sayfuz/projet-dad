17345ac36387c9d6609ba006c2d35b01469ce7d6
Sayfuz <mateo.r9756@gmail.com>
2021-02-09 22:41:55 -0500

    primer commir projet-dad carousel integrado

---
+++ src/routes/index.js
@@ -0,0 +1,21 @@
+const e = require('express')
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    res.render('index.html', {title: 'Publi White Medellín'})
+})
+
+router.get('/quienes-somos-publiwhite', (req, res) => {
+    res.render('about.html', {title: 'Quienes somos - Publi White Medellín'})
+})
+
+router.get('/servicios-somos-publiwhite', (req, res) => {
+    res.render('services.html', {title: 'Servicios - Publi White Medellín'})
+})
+
+router.get('/contacto-somos-publiwhite', (req, res) => {
+    res.render('contact.html', {title: 'Contacto - Publi White Medellín'})
+})
+
+module.exports = router
